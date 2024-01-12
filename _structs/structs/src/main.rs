f91bfb429b363f1c8b56990609c09671d485970d
dtex-farhan <farhan.ghazi@dtexddc.com>
2024-01-12 15:13:53 +1100

    added settings json to help with manual discovery of rust projects

--- _structs/structs/src/main.rs
+++ _structs/structs/src/main.rs
@@ -1,3 +1,34 @@
+// A "Tuple Struct" - a struct without named fields
+// to create different types
+struct Location(i32, i32, i32);
+
+struct User {
+    active: bool,
+    // Notice the types for "username" and "email".
+    // The choice of "String" type is deliberate -- we
+    // want instances of this struct to OWN all of its
+    // data.
+    username: String,
+    email: String,
+    sign_in_count: u64,
+    location: Location,
+    // This is an interesting one
+}
+
+fn create_user(email: String, username: String) -> User {
+    User {
+        active: true,
+        sign_in_count: 0,
+        location: Location(0, 0, 0),
+        email,
+        username,
+    }
+}
+
 fn main() {
+    let user1 = create_user(
+        String::from("farhansghazi@outlook.com"),
+        String::from("farhanghazi97"),
+    );
     println!("Hello, world!");
 }

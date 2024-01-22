c7774e0780e005a3ab12588e2157ee1cfcbd645f
dtex-farhan <farhan.ghazi@dtexddc.com>
2024-01-23 10:52:49 +1100

    completed implementing minigrep

--- checkpoint1/minigrep/src/main.rs
+++ checkpoint1/minigrep/src/main.rs
@@ -1,30 +1,17 @@
-use std::{env, fs};
+use minigrep::Config;
+use std::{env, process};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = parse_config(&args);
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        // Prints errors to standard error stream
+        eprintln!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
 
-    println!(
-        "Searching for '{}' in the file '{}'",
-        config.query, config.file_path
-    );
-
-    let contents =
-        fs::read_to_string(config.file_path).expect("Should have been able to read the file.");
-    println!("With text:\n{contents}");
-}
-
-struct Config {
-    query: String,
-    file_path: String,
-}
-
-fn parse_config(args: &Vec<String>) -> Config {
-    let query = &args[1];
-    let file_path = &args[2];
-    Config {
-        query: query.to_string(),
-        file_path: file_path.to_string(),
+    if let Err(err) = minigrep::run(config) {
+        eprintln!("Application error: {err}");
+        process::exit(1);
     }
 }

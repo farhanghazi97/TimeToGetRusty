5dcb39190aef94f25fc0f2d61d331537a599efa6
dtex-farhan <farhan.ghazi@dtexddc.com>
2024-01-30 09:32:11 +1100

    added new library crate to workspace

--- cargo/workspaces/adder/src/main.rs
+++ cargo/workspaces/adder/src/main.rs
@@ -1,7 +1,8 @@
 use add_one;
-use rand;
+use add_two;
 
 fn main() {
     let num = 10;
     println!("Hello, world! {num} + 1 is {}!", add_one::add_one(num));
+    println!("Hello, world! {num} + 2 is {}!", add_two::add_two(num));
 }
